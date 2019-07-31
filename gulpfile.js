09c6b9f26a3e1c3798003edc03a92c3f77730007
illidan970 <illidan970@abv.bg>
2019-07-31 18:43:53 +0300

    123145

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task("jekylldev", function() {
 
 // Jekyll
 gulp.task("jekyllprod", function() {
-	return cp.spawn("bundle", ["exec", "jekyll", "build --baseurl /еxam-project-master"], { stdio: "inherit", shell: true });
+	return cp.spawn("bundle", ["exec", "jekyll", "build --baseurl /Exam-project"], { stdio: "inherit", shell: true });
 });
 
 gulp.task("watch", function() {
