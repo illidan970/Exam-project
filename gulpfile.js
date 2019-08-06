bdfeec0e1703b99a74007df65e3c3606d07a31d7
illidan970 <illidan970@abv.bg>
2019-08-06 16:33:59 +0300

    22

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,8 @@ let gulp = require("gulp"),
 	cp = require("child_process");
 
 gulp.task("css", function() {
-	return gulp.src( '_assets/css/**/*.css' )
-		// .pipe( sass().on('error', sass.logError) )
+	return gulp.src( '_assets/scss/**/*.scss' )
+	    .pipe( sass().on('error', sass.logError) )
 		.pipe( autoprefixer() )
 		.pipe( gulp.dest( './docs/css/' ) )
 		.pipe( browserSync.stream({ match: '**/*.css' }) )
